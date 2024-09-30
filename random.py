410bcf5d3c6f112ad302f29c117618a1ac2017fb
sheela9492 <karpagasheela@outlook.com>
2024-09-30 12:51:57 +0100

    Update random.py

---
+++ random.py
@@ -0,0 +1,20 @@
+import random
+import my_module
+
+#random_integer = random.randint(1 , 10)
+#print(random_integer)
+
+#print(my_module. my_favorite_food)
+#print(my_module.my_favorite_colour)
+
+#random_float_0_t0_2 = random.random()*100
+#print(random_float_0_t0_2)
+
+#random_float = random.uniform(1 ,10)
+#print(random_float)
+import random
+random_number = random.randint(0, 1)
+if random_number == 0:
+    print("Head")
+else:
+    print("Tail")
